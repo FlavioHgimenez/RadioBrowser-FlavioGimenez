f8aee7517e0153adce12135f59c928a106988523
Daniel Hermenegildo Gimenez <attackhu3@gmail.com>
2023-06-23 17:03:59 -0300

    fix responsividade de editar

--- my-app/src/componets/Home/ListFavoriteRadios/ListFavoriteHome.tsx
+++ my-app/src/componets/Home/ListFavoriteRadios/ListFavoriteHome.tsx
@@ -36,19 +36,19 @@ const ListFavoriteHome = () => {
                               <h2 className="text-[white]">{getCurrentRadioName()}</h2>
                         </section>
                   )}
-                  <ul className="max-h-full overflow-y-scroll flex flex-col p-4 gap-2">
+                  <ul className="max-h-full overflow-y-scroll flex flex-col p-2 lg:p-4 gap-2">
                         {
                               favoriteRadio?.map((favItem: IDataRadiosItem, index: number) => (
                                     <li key={index} className="bg-white p-3 flex justify-between items-center shadow-md rounded-xl transition-opacity duration-700 delay-100 animate-opacity">
-                                          <div className="flex items-center gap-3">
+                                          <div className="flex items-center gap-1 lg:gap-3">
                                                 {favItem.url !== radioUrl &&
                                                       (
                                                             <button
                                                                   disabled={isLoading}
-                                                                  className={`aspect-square rounded-full p-2 cursor-pointer h-full bg-[#1b1b1b] ${isLoading ? "bg-gray-400" : "bg-[#1b1b1b]"}`}
+                                                                  className={`aspect-square rounded-full p-1 lg:p-2 cursor-pointer h-full bg-[#1b1b1b] ${isLoading ? "bg-gray-400" : "bg-[#1b1b1b]"}`}
                                                                   onClick={() => play(favItem.url)}
                                                             >
-                                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-6 h-6">
+                                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-5 h-5">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
                                                                   </svg>
                                                             </button>
@@ -57,10 +57,10 @@ const ListFavoriteHome = () => {
                                                       (
                                                             <button
                                                                   disabled={isLoading}
-                                                                  className={`aspect-square rounded-full p-2 cursor-pointer h-full  ${isLoading ? "bg-gray-200" : "bg-[#1b1b1b]"}`}
+                                                                  className={`aspect-square rounded-full p-1 lg:p-2 cursor-pointer h-full  ${isLoading ? "bg-gray-200" : "bg-[#1b1b1b]"}`}
                                                                   onClick={stop}
                                                             >
-                                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-6 h-6">
+                                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-5 h-5 ">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
                                                                   </svg>
                                                             </button>
@@ -71,7 +71,7 @@ const ListFavoriteHome = () => {
                                                             <input
                                                                   type="text"
                                                                   value={newName}
-                                                                  className="bg-white shadow border rounded-l-full px-2"
+                                                                  className="bg-white shadow border w-full rounded-l-full lg:px-2"
                                                                   onChange={(e) => { setNewName(e.target.value) }}
                                                             />
                                                             <button
@@ -83,26 +83,28 @@ const ListFavoriteHome = () => {
                                                             </button>
                                                       </div>
                                                 ) || (
-                                                            <h1>{favItem.name && favItem.name.replaceAll(" ", "").length > 0 ? favItem.name : "Sem Nome"}</h1>
+                                                            <p className="text-[12px] lg:text-[14px]">
+                                                                  {favItem.name && favItem.name.replaceAll(" ", "").length > 0 ? favItem.name : "Sem Nome"}
+                                                            </p>
                                                       )}
                                           </div>
                                           <div className="flex gap-3">
                                                 <button
-                                                      className="p-2 rounded-lg bg-[#e3e3e3] hover:bg-[#d7d7d7]"
+                                                      className="p-1 lg:p-2 rounded-lg bg-[#e3e3e3] hover:bg-[#d7d7d7]"
                                                       onClick={() => startEdition(favItem.stationuuid, favItem.name)}
                                                 >
-                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 lg:w-5 lg:h-5">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                                                       </svg>
                                                 </button>
                                                 <button
-                                                      className="p-2 rounded-lg bg-[#e3e3e3] hover:bg-[#d7d7d7]"
+                                                      className="p-1 lg:p-2 rounded-lg bg-[#e3e3e3] hover:bg-[#d7d7d7]"
                                                       onClick={(e) => {
                                                             removeFavorite(favItem.stationuuid)
 
                                                       }}
                                                 >
-                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 lg:w-5 lg:h-5">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                       </svg>
                                                 </button>
